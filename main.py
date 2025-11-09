c62d0ec111ce7395de2b165170998cd1e8cff2bb
no99 <hearttlessed@gmail.com>
2025-11-09 18:23:12 +0700

    rename folder

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-import sqlite3
-con = sqlite3.connect('films_db.sqlite')
-cur = con.cursor()
-app = FastAPI()
-@app.get("/")
-async def roma():
-    return [i for i in cur.execute('select * from films where id < 51')]
